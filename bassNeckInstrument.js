7583c0db7b4da60803cf337bbbd312ef91975ebc
Andrew  Pacheco <ndrewpacheco@gmail.com>
2022-02-19 14:31:18 -0500

    notes being played with pointer events and tone.js

--- bassNeckInstrument.js
+++ bassNeckInstrument.js
@@ -1,25 +1,28 @@
-import * as Tone from "tone";
-
 const synth = new Tone.Synth().toDestination();
 
 const main = document.querySelector(".main");
 const bassNotes = [
-  ["E1", "A1", "D1", "G2"],
-  ["F1", "A#1", "D#1", "G#2"],
+  ["E1", "A1", "D2", "G2"],
+  ["F1", "A#1", "D#2", "G#2"],
   ["F#1", "B1", "E2", "A2"],
-  ["G1", "C1", "F2", "A#2"],
-  ["G#1", "C#1", "F#2", "B2"],
+  ["G1", "C2", "F2", "A#2"],
+  ["G#1", "C#2", "F#2", "B2"],
 ];
 
 bassNotes.forEach((bassFret) => {
   const fret = document.createElement("div");
-  fret.setAttribute("class", "fret");
+  fret.className = "fret";
 
   bassFret.reverse().forEach((letter) => {
     const note = document.createElement("div");
-    note.setAttribute("class", "note");
+    note.className = "note";
     note.textContent = letter;
 
+    note.addEventListener("pointerdown", (evt) => {
+      console.log("touched " + letter);
+      synth.triggerAttackRelease(letter, "16n");
+      evt.preventDefault();
+    });
     fret.appendChild(note);
   });
 
